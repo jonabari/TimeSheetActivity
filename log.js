eaf2c8764c673158f224e6f0572514766c60dc8e
jonabari <barcelo.jonathan@gmail.com>
2018-09-29 11:05:30 -0500

    yaaarp

---
+++ log.js
@@ -0,0 +1,16 @@
+
+
+
+  var config = {
+    apiKey: "AIzaSyAXoA8ngJIRBD7V-TQUJ1a2ma1R4C9Uy4k",
+    authDomain: "timesheetactivity-73196.firebaseapp.com",
+    databaseURL: "https://timesheetactivity-73196.firebaseio.com",
+    projectId: "timesheetactivity-73196",
+    storageBucket: "timesheetactivity-73196.appspot.com",
+    messagingSenderId: "770080722872"
+  };
+  firebase.initializeApp(config);
+
+$ ( "#submit-btn" ) .on ( "click" , function () {
+    console.log ( "i hear ya!" ) ;
+} ) ;
