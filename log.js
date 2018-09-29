6bf878418a8a77e459a64654551d6ecce121c7fc
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-29 11:43:27 -0500

    New iteration.

--- log.js
+++ log.js
@@ -1,16 +1,44 @@
+// Initialize Firebase
 
+var config = {
+  apiKey: "AIzaSyDxZ1HlsQYpkctf-DadU7DxeL_b1nVYrNQ",
+  authDomain: "fir-db-1df80.firebaseapp.com",
+  databaseURL: "https://fir-db-1df80.firebaseio.com",
+  projectId: "fir-db-1df80",
+  storageBucket: "fir-db-1df80.appspot.com",
+  messagingSenderId: "21291710553"
+};
+firebase.initializeApp(config);
 
+var database = firebase.database();
 
-  var config = {
-    apiKey: "AIzaSyAXoA8ngJIRBD7V-TQUJ1a2ma1R4C9Uy4k",
-    authDomain: "timesheetactivity-73196.firebaseapp.com",
-    databaseURL: "https://timesheetactivity-73196.firebaseio.com",
-    projectId: "timesheetactivity-73196",
-    storageBucket: "timesheetactivity-73196.appspot.com",
-    messagingSenderId: "770080722872"
-  };
-  firebase.initializeApp(config);
+// INSERT INITIAL VALUES HERE
+
+var connectionsRef = database.ref("/connections");
+
+var connectedRef = database.ref(".info/connected");
+
+connectedRef.on("value", function(snap) {
+
+  if (snap.val()) {
+
+    var con = connectionsRef.push(true);
+
+    con.onDisconnect().remove();
+  }
+});
 
 $ ( "#submit-btn" ) .on ( "click" , function () {
-    console.log ( "i hear ya!" ) ;
+
+  var name = $("#name-input-fb").val().trim();
+  var role = $("#role-input-fb").val().trim();
+
+  event.preventDefault();
+  console.log ( "i hear ya!" ) ;
+
+    database.ref().push({
+      name: name,
+      role: role
+    });
+
 } ) ;
